060bc7a2839bf6749a6306e973bba81fcd315f46
d-fresh <dghicken@gmail.com>
2016-02-16 19:04:03 -0700

    Moved folders

---
+++ mini-userProfiles II/service.js
@@ -0,0 +1,17 @@
+var app = angular.module('userProfiles'); //wow
+
+app.service('mainService', function($http) {
+
+  this.getUsers = function() {
+    return $http({
+			method: 'GET',
+			url: 'http:/regres.in/api/users/?page=1',
+		}).then(function(response) {
+			if(response.status === 200){
+				return response;
+			}
+		})
+  };
+
+
+});
